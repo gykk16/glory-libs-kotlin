1f3e0cf1076922742a3deabf7999a9ca875a0140
ykk <gykk16@gmail.com>
2025-03-08 17:05:00 +0900

    update to spring boot 3.4.3

--- core/core-mvc/build.gradle.kts
+++ core/core-mvc/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
     kotlin("plugin.jpa") version "2.0.0"
